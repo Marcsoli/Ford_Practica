8c8c9e3cf6e6d3dc373c4f1ff1ae70fa76b19a73
Marcos Solis <Marcos Solis@192.168.0.29>
2023-10-03 18:38:50 -0600

    Practica 2 de Ford

---
+++ src/main/java/com/Ford/FordApplication.java
@@ -0,0 +1,13 @@
+package com.Ford;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FordApplication.class, args);
+	}
+
+}
